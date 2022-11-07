ebd65550d80c7955961e68f5e982feb2d48069a4
Jean Cesca <jeanrcesca@gmail.com>
2022-11-07 14:24:39 -0300

    Profile DataForm UI

--- TwitterClone/Controllers /General/ProfileDataFormViewController.swift
+++ TwitterClone/Controllers /General/ProfileDataFormViewController.swift
@@ -11,10 +11,109 @@ import UIKit
 
 class ProfileDataFormViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.alwaysBounceVertical = true
+        scrollView.keyboardDismissMode = .onDrag
+        return scrollView
+    }()
+    
+    private let displayNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.keyboardType = .default
+        textField.backgroundColor = .secondarySystemFill
+        textField.leftViewMode = .always
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 8
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "Display Name",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        return textField
+    }()
+    
+    private let userNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.keyboardType = .default
+        textField.backgroundColor = .secondarySystemFill
+        textField.leftViewMode = .always
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 8
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "Username",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        return textField
+    }()
+    
+    private let hintLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Fill in your data"
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.textColor = .label
+        return label
+    }()
+    
+    private let avatarPracebolderImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 60
+        imageView.backgroundColor = .lightGray
+        imageView.image = UIImage(systemName: "camera.fill")
+        imageView.tintColor = .gray
+        imageView.isUserInteractionEnabled = true
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        isModalInPresentation = true
+        
+        view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+        scrollView.addSubview(hintLabel)
+        scrollView.addSubview(avatarPracebolderImageView)
+        scrollView.addSubview(displayNameTextField)
+        scrollView.addSubview(userNameTextField)
+        
+        configureConstraints()
     }
+    
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            hintLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            hintLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 30),
+            
+            avatarPracebolderImageView.heightAnchor.constraint(equalToConstant: 120),
+            avatarPracebolderImageView.widthAnchor.constraint(equalToConstant: 120),
+            avatarPracebolderImageView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            avatarPracebolderImageView.topAnchor.constraint(equalTo: hintLabel.bottomAnchor, constant: 30),
+            
+            displayNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            displayNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            displayNameTextField.topAnchor.constraint(equalTo: avatarPracebolderImageView.bottomAnchor, constant: 40),
+            displayNameTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            userNameTextField.leadingAnchor.constraint(equalTo: displayNameTextField.leadingAnchor),
+            userNameTextField.trailingAnchor.constraint(equalTo: displayNameTextField.trailingAnchor),
+            userNameTextField.topAnchor.constraint(equalTo: displayNameTextField.bottomAnchor, constant: 20),
+            userNameTextField.heightAnchor.constraint(equalTo: displayNameTextField.heightAnchor),
+        ])
+    }
+    
+    
     
 }
